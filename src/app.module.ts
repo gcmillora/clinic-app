bd0d6a24a076631c745d3f87adafe3276bbb2905
Greg Millora <gregmillora.dev@gmail.com>
2024-01-29 22:10:44 +0800

    added config/prisma/user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ClinicsModule } from './clinics/clinics.module';
-import { AppointmentsService } from './appointments/appointments.service';
 import { AppointmentsController } from './appointments/appointments.controller';
 import { AppointmentsModule } from './appointments/appointments.module';
+import { AppointmentsService } from './appointments/appointments.service';
 import { BookingsModule } from './bookings/bookings.module';
+import { ClinicsModule } from './clinics/clinics.module';
+import configuration from './config/configuration';
 import { TimeslotsModule } from './timeslots/timeslots.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,6 +18,9 @@ import { TimeslotsModule } from './timeslots/timeslots.module';
     AppointmentsModule,
     BookingsModule,
     TimeslotsModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
   ],
   controllers: [AppController, AppointmentsController],
   providers: [AppService, AppointmentsService],
